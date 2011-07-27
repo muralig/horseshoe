e59c970a9359907b3be05d55de6a657f2e6a8913
sdamaraj <sdamaraj@Sdamaraj-Win7.adobenet.global.adobe.com>
2011-07-26 19:10:36 -0700

    initial project commit.

---
+++ horseshoe/src/fivehour/main.py
@@ -0,0 +1,24 @@
+'''
+Created on Jul 26, 2011
+
+@author: sdamaraj
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        pass
+
+if __name__ == '__main__':
+    print "Hello World!"
+    
+       
+
+        
