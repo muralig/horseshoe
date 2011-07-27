3cd6da4598410de26db18242ddc5d33988d60093
sdamaraj <sdamaraj@Sdamaraj-Win7.adobenet.global.adobe.com>
2011-07-27 00:21:29 -0700

    Get JSON

--- horseshoe/src/fivehour/main.py
+++ horseshoe/src/fivehour/main.py
@@ -3,6 +3,8 @@ Created on Jul 26, 2011
 
 @author: sdamaraj
 '''
+import urllib
+import json
 
 import kivy
 #kivy.require('1.0.7')
@@ -10,20 +12,24 @@ import kivy
 from kivy.app import App
 from kivy.uix.button import Button
 
+url = "http://www.reddit.com/r/fffffffuuuuuuuuuuuu/top/.json"
     
 def buttonPressed(params):
     print "Pressed Button", params
 
 def buttonReleased(params):
     print "Relased Button", params
+    theJsonUrl = urllib.urlopen(url)
+    jsonString = theJsonUrl.read()
+    theJsonUrl.close()
+    print jsonString
+    
 
 class MyClass(App):
     '''
     classdocs
     '''
-    
 
-            
     def build(self):
         self.button = Button(text='Hello World')
         self.button.bind(on_press=buttonPressed)
